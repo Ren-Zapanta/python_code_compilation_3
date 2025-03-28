b52f8f315463cbab39408526504605c2edb1e7fe
Ren <renzapnta4@gmail.com>
2025-03-28 23:28:38 +0800

    Applies custom function the user's input and prints the fomatted text

--- batch_6/string_manipulation_lowercase.py
+++ batch_6/string_manipulation_lowercase.py
@@ -1,4 +1,5 @@
 #Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
 def make_lowercase(s):
     result = ""  #nitialize an empty string to store the result
     for char in s:  #LOop through each character in the input string
@@ -7,3 +8,12 @@ def make_lowercase(s):
         else:
             result += char  #if not uppercase, keep the character unchanged
     return result  #return the lowercase converted string
+
+#Request input from user
+user_input = input("Lagay ka po statement, sir: ")
+
+#Applies custom function to user input
+apply_function = make_lowercase(user_input) 
+
+
+print(f"Formatted text: {apply_function}")  #Print the formatted text
