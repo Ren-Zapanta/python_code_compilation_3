7862b30741fab0430702fca53896c5799344410f
Ren <renzapnta4@gmail.com>
2025-03-29 02:18:01 +0800

    Requests user input. Splits user input to words. Initializes empty list.

---
+++ batch_6/string_manipulation_title.py
@@ -0,0 +1,7 @@
+#Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+user_input = input("Please enter a statement: ") #Asks the user for an input
+
+word = user_input.split() #splits the input 
+
+formatted_words = [] #initializes empty string
+
