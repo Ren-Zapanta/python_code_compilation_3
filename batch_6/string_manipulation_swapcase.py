b3d8c43fe75d61130ffb7c428e2ef050a8aa658c
Ren <renzapnta4@gmail.com>
2025-03-29 01:27:42 +0800

    requests user input

---
+++ batch_6/string_manipulation_swapcase.py
@@ -0,0 +1,4 @@
+#Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+user_input = input("Please enter a statement: ") #Requests input from user
+
+
