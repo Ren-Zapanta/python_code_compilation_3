2d81c033a202908ea6571583a6085385e576f4d3
Ren <renzapnta4@gmail.com>
2025-03-29 01:52:40 +0800

    Printes inverted case characters

--- batch_6/string_manipulation_swapcase.py
+++ batch_6/string_manipulation_swapcase.py
@@ -1,4 +1,16 @@
 #Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 user_input = input("Please enter a statement: ") #Requests input from user
 
+formatted_text = "" #Creates an empty string for the result
+
+for char in user_input: #loops through the characters
+    if char.islower(): #converts lowercase to upper case
+        formatted_text += char.upper()
+    elif char.isupper(): #converts uppercase to lower case
+        formatted_text += char.lower()  
+    else: 
+        formatted_text += char #adds characters back without converting
+
+print(formatted_text) #Prints result
+        
 
