03e015ebeb9e50626ab19a16e81c0550cb6030d1
Ren <renzapnta4@gmail.com>
2025-03-29 01:02:42 +0800

    Requests user input and desired length

---
+++ batch_6/string_manipulation_center().py
@@ -0,0 +1,6 @@
+#Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+user_input_string = input("Lagay ka string, sir: ") #Requests string input from user
+user_input_length = int(input("Gaano kahaba mo po gusto, sir?: ")) #Requests desired user length
+
+get_length = len(user_input_string) #gets the length of user's input string
+
