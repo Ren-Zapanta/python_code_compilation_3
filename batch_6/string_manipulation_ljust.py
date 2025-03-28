a8051bf0a506d870397c9d48a5d693751a702d53
Ren <renzapnta4@gmail.com>
2025-03-29 00:40:48 +0800

    requests string and desired string length from user

---
+++ batch_6/string_manipulation_ljust.py
@@ -0,0 +1,6 @@
+#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+user_input_string = input("Lagay ka string, sir: ") #Requests string input from user
+user_input_length = int(input("Gaano kahaba mo po gusto, sir?: ")) #Requests desired user length
+get_length = len(user_input_string) #gets the length of user's input string
+
+
