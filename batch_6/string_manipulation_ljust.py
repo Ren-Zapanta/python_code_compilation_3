4b3c888420d7d0422e50a3e59cb90b1ba02394a8
Ren <renzapnta4@gmail.com>
2025-03-29 00:47:53 +0800

    Prints result

--- batch_6/string_manipulation_ljust.py
+++ batch_6/string_manipulation_ljust.py
@@ -3,4 +3,10 @@ user_input_string = input("Lagay ka string, sir: ") #Requests string input from 
 user_input_length = int(input("Gaano kahaba mo po gusto, sir?: ")) #Requests desired user length
 get_length = len(user_input_string) #gets the length of user's input string
 
+if get_length < user_input_length: #checks which is bigger between desired length and actual string length
+    padding = " " * (user_input_length - get_length)  #Creates the required spaces to match the desired length
+    output_string = user_input_string + padding # Concatenates the original string with spaces
+else:
+    output_string = user_input_string
 
+print(output_string) #Prints result
