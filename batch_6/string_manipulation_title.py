eb9b79a30d030999e2908a11206eddbae49cda89
Ren <renzapnta4@gmail.com>
2025-03-29 02:24:05 +0800

    loops through the words, capitalizes each first letter. Joins them back together, and prints it

--- batch_6/string_manipulation_title.py
+++ batch_6/string_manipulation_title.py
@@ -1,7 +1,16 @@
 #Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 user_input = input("Please enter a statement: ") #Asks the user for an input
 
-word = user_input.split() #splits the input 
+words = user_input.split() #splits the input 
 
 formatted_words = [] #initializes empty string
 
+for word in words: #loops through each word and capitalizes every first letter
+    if word: #Verifies if the word is empty
+
+        formatted_word = word[0].upper() + word[1:].lower()
+        formatted_words.append(formatted_word)
+
+formatted_text = " ".join(formatted_words) #Joins back the words
+
+print(formatted_text) #Prints result
