c6aa27e8b549dcd977e113b932d6cf77f9908f5b
Ren <renzapnta4@gmail.com>
2025-03-29 00:26:34 +0800

    gets the length of the sample suffix. compares sample suffix with the last part of the string. compares last part of the string variable with sample suffix

--- batch_6/string_manipulation_endswith.py
+++ batch_6/string_manipulation_endswith.py
@@ -2,3 +2,11 @@
 sample_text = "For inspiration lang" #Sample string 
 sample_suffix = "lang" #Sample suffix
 
+suffix_length = len(sample_suffix) #Gets suffix length
+last_part = sample_text[-suffix_length:] #Gets the last part of the string 
+
+if last_part == sample_suffix: #Compares the ending part with sample_suffix variable
+#prints corresponding result
+    print("True")
+else:
+    print("False")
