983367aa7061579ee03504e3c4a2bb742a5c32fb
Ren <renzapnta4@gmail.com>
2025-03-28 23:14:30 +0800

    Applies custom function to a given string.

--- batch_6/string_manipulation_prefix.py
+++ batch_6/string_manipulation_prefix.py
@@ -4,3 +4,13 @@ def custom_removeprefix(string, prefix):
     if string.startswith(prefix):  # If the prefix is found at the beginning
         return string[len(prefix):]  # Remove the prefix by slicing the string
     return string  # If no prefix match, return the original string
+
+original_string = "Ungrateful sa grades" #Originial String
+prefix_to_remove = "Un" #Specified prefix
+
+
+formatted_string = custom_removeprefix(original_string, prefix_to_remove) #Calls the custom function
+
+# Display the result
+print(f"Original string: {original_string}")  # Print the original string
+print(f"Formatted string: {formatted_string}")  # Print the modified string without the prefix
