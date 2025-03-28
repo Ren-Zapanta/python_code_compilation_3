8f7c8c4415692e075bee9a76ce97c1612d506058
Ren <renzapnta4@gmail.com>
2025-03-29 01:13:03 +0800

    Verifies the need for padding of the string, calculates for the required padding for both left and right and prints the padded string

--- batch_6/string_manipulation_center().py
+++ batch_6/string_manipulation_center().py
@@ -4,3 +4,13 @@ user_input_length = int(input("Gaano kahaba mo po gusto, sir?: ")) #Requests des
 
 get_length = len(user_input_string) #gets the length of user's input string
 
+if get_length < user_input_length:  #Verifies for the need of "padding"
+    total_padding = user_input_length - get_length  #gets the total space needed
+    left_padding = total_padding // 2  #Spaces for the left
+    right_padding = total_padding - left_padding  #Spaces for the right
+
+    output_string = (" " * left_padding) + user_input_string + (" " * right_padding)  # Construct the centered string
+else:
+    output_string = user_input_string  #long enough
+
+print(f"'{output_string}'")  #print output, indicate spaces w/ single quotes
